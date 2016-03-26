06973cb06b8ecc4dc383f78309fa579b15275696
Matt Poegel <mpoegel@comcast.net>
2016-03-25 23:36:58 -0400

    send house name before list of apis

--- source/controllers/index.js
+++ source/controllers/index.js
@@ -8,28 +8,28 @@ var express = require('express'),
 router.get('/idea/web', function(req, res) {
   var idea = ideas.getIdea('web');
   console.log(idea);
-  hatComm.sendMessage( idea.join(', ') );
+  hatComm.sendMessage( 'Gryffindor ' + idea.join(', ') );
   res.send('ok');
 });
 
 router.get('/idea/desktop', function(req, res) {
   var idea = ideas.getIdea('desktop');
   console.log(idea);
-  hatComm.sendMessage( idea.join(', ') );
+  hatComm.sendMessage( 'Hufflepuff ' + idea.join(', ') );
   res.send('ok');
 });
 
 router.get('/idea/mobile', function(req, res) {
   var idea = ideas.getIdea('mobile');
   console.log(idea);
-  hatComm.sendMessage( idea.join(', ') );
+  hatComm.sendMessage( 'Slytherin ' + idea.join(', ') );
   res.send('ok');
 });
 
 router.get('/idea/data_science', function(req, res) {
   var idea = ideas.getIdea('data_science');
   console.log(idea);
-  hatComm.sendMessage( idea.join(', ') );
+  hatComm.sendMessage( 'Ravenclaw ' + idea.join(', ') );
   res.send('ok');
 });
 
